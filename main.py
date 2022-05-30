4325bdfb70c035f356bab1f57bfc23eaa9010366
Lydia <lyklime@gmail.com>
2022-05-30 20:45:03 +0400

    translate all massage to russian;

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import telebot
+import translators as ts
 
 bot = telebot.TeleBot('5414940662:AAHldK9P55Wn4HR60-pM2J04VlvYEutgMHY')
 
 
 @bot.message_handler(content_types=['text', 'document'])
 def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+    bot.send_message(message.from_user.id, ts.google(message.text, from_language='auto', to_language='ru'))
 
 
 bot.polling(none_stop=True, interval=0)
