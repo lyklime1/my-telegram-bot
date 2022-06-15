59906b07931b1f82614804e16575f758e79a93b2
Lydia <lyklime@gmail.com>
2022-06-16 02:07:20 +0400

    fix import paths in client.py and other.py; update gitignore;

--- handlers/client.py
+++ handlers/client.py
@@ -1,9 +1,9 @@
 from aiogram import types, Dispatcher
-from telegram import create_bot
+from create_bot import bot
 
 
 async def start(message: types.Message):
-    await create_bot.bot.send_message(message.chat.id, 'Hello! I will translate any massage to any language')
+    await bot.send_message(message.chat.id, 'Hello! I will translate any massage to any language')
 
 
 def reg_handlers_client(dp: Dispatcher):
--- handlers/other.py
+++ handlers/other.py
@@ -1,27 +1,27 @@
 import pytesseract
 import translators as ts
 from PIL import Image
-from telegram import create_bot
+from create_bot import bot
 from os import remove
 from aiogram import types, Dispatcher
 
 
 async def get_text_messages(message: types.message):
-    await create_bot.bot.send_message(message.chat.id, ts.google(message.text, from_language='auto', to_language='ru'))
+    await bot.send_message(message.chat.id, ts.google(message.text, from_language='auto', to_language='ru'))
 
 
 async def get_photo_messages(message: types.message):
     raw = message.photo[2].file_id
     name = raw + ".jpg"
-    file_info = create_bot.bot.get_file(raw)
-    downloaded_file = create_bot.bot.download_file(file_info.file_path)
+    file_info = bot.get_file(raw)
+    downloaded_file = bot.download_file(file_info.file_path)
     with open(name, 'wb') as new_file:
         new_file.write(downloaded_file)
     img = Image.open(name)
     pytesseract.pytesseract.tesseract_cmd = r'C:\Users\lida_\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
     result = pytesseract.image_to_string(img)
-    await create_bot.bot.send_message(message.from_user.id, result)
-    await create_bot.bot.send_message(message.from_user.id, ts.google(result, from_language='auto', to_language='ru'))
+    await bot.send_message(message.from_user.id, result)
+    await bot.send_message(message.from_user.id, ts.google(result, from_language='auto', to_language='ru'))
     remove(name)
 
 
